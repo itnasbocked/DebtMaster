8c8d8c0d5bcd94171bb3e486e274c5974b8be2bb
itnasbocked <floresbetancourtsantiagojacob@gmail.com>
2026-02-23 13:15:36 -0600

    Lógica de ingresos, egresos, y su comunicación con ui y bd

---
+++ lib/data/database/database_helper.dart
@@ -0,0 +1,127 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/ingresos-egresos.dart';
+
+class DatabaseHelper {
+
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  // Obtener la base de datos
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('debtmaster.db');
+    return _database!;
+  }
+
+  // Inicializar DB
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    // 1. Tabla de Usuarios (Monto en INTEGER)
+    await db.execute('''
+      CREATE TABLE usuario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        ingreso_mensual INTEGER NOT NULL
+      )
+    ''');
+
+    // 2. Tabla de Tarjetas (Monto en INTEGER)
+    await db.execute('''
+      CREATE TABLE tarjeta (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario_id INTEGER NOT NULL,
+        nombre_tarjeta TEXT,
+        tipo TEXT,
+        corte_dia INTEGER,
+        pago_dia INTEGER,
+        monto_minimo INTEGER,
+        FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+      )
+    ''');
+
+    // 3. Tabla de Gastos Fijos (Monto en INTEGER)
+    await db.execute('''
+      CREATE TABLE gasto_fijo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario_id INTEGER NOT NULL,
+        nombre_gasto TEXT,
+        monto INTEGER,
+        frecuencia TEXT,
+        fecha_pago TEXT,
+        FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+      )
+    ''');
+
+    // 4. NUEVA: Tabla de Movimientos (Ingresos/Egresos)
+    await db.execute('''
+      CREATE TABLE movimiento (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario_id INTEGER NOT NULL,
+        monto INTEGER NOT NULL,
+        fecha TEXT NOT NULL,
+        tipo TEXT NOT NULL,
+        descripcion TEXT,
+        frecuencia TEXT DEFAULT 'ninguna', -- 'diario', 'semanal', 'mensual', etc.
+        FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+      )
+    ''');
+
+    // 5. Tabla de Alertas
+    await db.execute('''
+      CREATE TABLE alerta (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario_id INTEGER NOT NULL,
+        tipo TEXT,
+        fecha_alerta TEXT,
+        mensaje TEXT,
+        FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+        )
+    ''');
+  
+  }
+
+  Future<int> crearMovimiento(Movimiento movimiento) async{
+    final db = await instance.database;
+    return await db.insert('movimiento', movimiento.toMap());
+  }
+
+  Future<List<Movimiento>> consultarMovimientos(int usuario_id) async{
+    final db = await instance.database;
+    final result = await db.query('movimiento',
+    where: 'usuario_id = ?',
+    whereArgs: [usuario_id],
+    orderBy: 'fecha DESC');
+    return result.map((map) => Movimiento.fromMap(map)).toList();
+  }
+
+  Future<int> obtenerBalance(int usuarioId) async{
+    final Database db = instance.database as Database;
+
+    final resultIngresos = await db.rawQuery(
+      'SELECT SUM(monto) as total FROM movimiento WHERE usuario_id = ? and tipo = "ingreso"',
+      [usuarioId]
+    );
+    final resultEgresos = await db.rawQuery(
+      'SELECT SUM(monto) as total FROM movimiento WHERE usuario_id = ? and tipo = "egreso"',
+      [usuarioId]
+    );
+
+    int ingresos = resultIngresos.first['total'] as int ?? 0;
+    int egresos = resultEgresos.first['total'] as int ?? 0;
+
+    return ingresos - egresos;
+  }
+}
---
+++ lib/logic/movimiento_controller.dart
@@ -0,0 +1,46 @@
+import '../data/database/database_helper.dart';
+import '../data/models/ingresos-egresos.dart';
+
+class MovimientoController{
+  Future<String> registrarMovimiento({
+    required String montoRaw,
+    required String descripcion,
+    required String tipo,
+    required int usuarioId,
+  }) async{
+    try{
+      if (montoRaw.isEmpty) return 'El monto no puede estar vacío';
+      final montoDecimal = double.tryParse(montoRaw);
+      if (montoDecimal == null || montoDecimal <= 0) return 'Monto inválido. Debe ser un número positivo.';
+      int montoCentavos = (montoDecimal * 100).round();
+
+      final nuevoMovimiento = Movimiento(usuario_id: usuarioId, monto: montoCentavos, fecha: DateTime.now(), tipo: tipo,
+      descripcion: descripcion.isEmpty ? "Sin descripción" : descripcion, frecuencia: 'ninguna');
+
+      final id = await DatabaseHelper.instance.crearMovimiento(nuevoMovimiento);
+      if (id > 0){
+        return 'ÉXITO';
+      } else{
+        return 'Error al registrar movimiento';
+      }
+
+    } catch (e){
+      return 'Error al registrar movimiento: ${e.toString()}';
+    }
+  }
+
+  Future<double> formatearBalance(int usuarioId) async{
+    int balanceCentavos = await DatabaseHelper.instance.obtenerBalance(usuarioId);
+    return balanceCentavos / 100.0;
+  }
+
+  Future <List<Movimiento>> obtenerMovimientos(int usuarioId) async{
+    try{
+      return await DatabaseHelper.instance.consultarMovimientos(usuarioId);
+    } catch (e){
+      //Devolver una lista vacía para no romper la UI
+      print('Error al obtener movimientos: ${e.toString()}');
+      return [];
+    }
+  }
+}
